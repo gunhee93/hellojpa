252ab7ce9bf2b207f629e32200fab5f1554fd1c8
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-03-21 18:05:13 +0900

    jpql
    -jpql

--- src/main/java/jpql/Product.java
+++ src/main/java/jpql/Product.java
@@ -1,2 +1,47 @@
-package jpql;public class Product {
+package jpql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private int price;
+    private int stockAmount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
 }
